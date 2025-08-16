d5eae38b721b19f8b6d946daf2e0f45b1cf27571
hamuzon <warabimoti@hamusata.f5.si>
2025-08-16 20:27:34 +0900

    link - Update - sites.js
    hamuzon.github.io 系の方の link pixel から pixel-art にlinkUpdate

--- sites.js
+++ sites.js
@@ -36,7 +36,7 @@ const sitesData = {
     { href: "https://hamuzon.github.io/omikuji/", text: "🍀おみくじ🔮 - Hamuzon", openInSameTab: true },
     { href: "https://hamuzon.github.io/dice/", text: "🎲サイコロ🎯  - Hamuzon", openInSameTab: true },
     { href: "https://hamuzon.github.io/Short-Link/", text: "🔗Short link creation service🔗  - Hamuzon", openInSameTab: true },
-    { href: "https://hamuzon.github.io/pixel/", text: "🎨 Pixel Drawing 🖌️  - Hamuzon", openInSameTab: true },
+    { href: "https://hamuzon.github.io/pixel-art/", text: "🎨 Pixel Drawing 🖌️  - Hamuzon", openInSameTab: true },
   ],
   publicSites: [
     { href: "https://www.google.com", text: "Google", openInSameTab: true },
