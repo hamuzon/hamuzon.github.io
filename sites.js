0f94a0cc4e791b104bbb3eb2a1c1054d2e7e8e7b
hamuzon <warabimoti@hamusata.f5.si>
2025-09-14 12:41:42 +0900

    link Update sites.js
    link google.hamusata.f5.si
    からhamusata.hamusata.f5.si

--- sites.js
+++ sites.js
@@ -2,7 +2,7 @@ const sitesData = {
   homeHamusata: [
     { href: "https://home.hamusata.f5.si/", text: "Hamusata-Website-List", openInSameTab: true },
     { href: "https://hamusata.f5.si/", text: "Hamusata-home-Page", openInSameTab: true },
-    { href: "https://google.hamusata.f5.si/", text: "Hamusata-home-Page^Google-site-VERSION", openInSameTab: true },
+    { href: "https://hamusata.hamusata.f5.si/", text: "Hamusata-home-Page^Google-site-VERSION", openInSameTab: true },
     { href: "https://scratch.hamusata.f5.si/", text: "Scratch User & Project Info / Run on Netlify, stored on GitHub.", openInSameTab: true },
     { href: "https://time.hamusata.f5.si", text: "Time Zone / Local PC Service by Cloudflare Tunnel", openInSameTab: true },
     { href: "https://time-zone.hamusata.f5.si", text: "Time Zone / Hosted by GitHub", openInSameTab: true },
